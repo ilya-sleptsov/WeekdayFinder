ab4dde105ecf41b96839133044c887d24c23abe3
ilya-sleptsov <79329934+ilya-sleptsov@users.noreply.github.com>
2023-04-07 15:28:12 +0300

    Add files via upload
    Initial commit

---
+++ WeekdayFinder/WeekdayFinder/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  WeekdayFinder
+//
+//  Created by Илья Слепцов on 06.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dayTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findWeekDay() {
+        let calendar = Calendar.current
+        
+        var dateComponents = DateComponents()
+        guard let day = dayTF.text, let month = monthTF.text, let year = yearTF.text else { return }
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        guard let date = calendar.date(from: dateComponents) else { return }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        let weekday = dateFormatter.string(from: date)
+        
+        resultLabel.text = weekday
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
+
